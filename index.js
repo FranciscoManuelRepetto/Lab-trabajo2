88d62c07e19cd9144f9c132af1972bf4a277468e
Jerexio <jeremiasrodriguez05@gmail.com>
2022-10-08 21:01:28 -0300

    Se agrega la capacidad de construir el HTML desde JS, usando un archivo .JSON

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 import menus from './menus.json' assert { type:'json' };
-const elemAlmuerzo = document.querySelector(".main-container");
+const elemAlmuerzo = document.querySelector(".conteiner");
 
 console.log(menus);
 
-let elem = menus["almuerzos"][0];
+const buildTodayDate = () => {
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = today.getFullYear();
+    today = dd + '/' + mm + '/' + yyyy;
+    return today;
+}
 
-let objectHTML = 
-"<div class=\"almuerzos\">" +
-elem.foto + "<br>" +
-elem.nombre + "<br>" +
-elem.ingredientes + "<br>" +
-elem.precioCarnet + "<br>" +
-elem.precio +
+let today = buildTodayDate();
+
+let elem = menus["desayunos"][0];
+
+let bigBox = 
+"<div class=\"caja-de-menu-grande\">"+
+"<img class=\"img-grande\" alt=\"Imagen del menu"+elem.nombre+"\""+
+"src=\""+elem.foto+"\">"+
+"<h4>"+elem.nombre+"</h4>"+
+"<p>"+
+elem.ingredientes+
+"</p>"+
+"<p>Precio: "+elem.precio+"</p>"+
+"<p>Precio Carnet: "+elem.precioCarnet+"</p>"+
+"<p>Fecha de hoy: "+today+"</p>"+
+"<button type =\"button\"><i class=\"bi bi-hand-thumbs-up\"></i></button>"+
 "</div>";
 
 
-elemAlmuerzo.innerHTML += objectHTML;
+elemAlmuerzo.innerHTML += bigBox;
